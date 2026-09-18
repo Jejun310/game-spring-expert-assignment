4c1f4679927af91ef460bfd5fa6d8d09ee870475
Jejun310 <jejunna@naver.com>
2026-09-18 19:12:49 +0900

    Fix:Lv.3 요ì²닉네임 ì 글자수, 정규식 및 DTO 검증과 성공 응답 구현

--- src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
+++ src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
@@ -1,13 +1,16 @@
 package com.gameexpert.player.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreatePlayerRequest {
 
     // TODO Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증을 적용합니다.
+    @Size(min = 2, max = 12)
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$")
     private final String nickname;
-
     public CreatePlayerRequest(String nickname) {
         this.nickname = nickname;
     }
