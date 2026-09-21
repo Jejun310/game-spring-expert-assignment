b102b050400b65323d49c8dcb84d12157bb4183c
Jejun310 <jejunna@naver.com>
2026-09-21 17:25:52 +0900

    Fix:Lv.11 메시지 라우팅과 Ping/Pong 응답 구현

--- src/main/java/com/gameexpert/ws/NicknameHandshakeInterceptor.java
+++ src/main/java/com/gameexpert/ws/NicknameHandshakeInterceptor.java
@@ -73,7 +73,7 @@ public class NicknameHandshakeInterceptor implements HandshakeInterceptor {
 
         // TODO Lv 7: nickname과 worldId를 ATTR_NICKNAME, ATTR_WORLD_ID 키로 attributes에 저장합니다.
         attributes.put(ATTR_NICKNAME, nickname);
-        attributes.put(ATTR_WORLD_ID, world);
+        attributes.put(ATTR_WORLD_ID, worldId);
 
         attributes.put(ATTR_PLAYER_ID, player.getId());
         attributes.put(ATTR_WORLD_SEED, (int) world.getSeed());
